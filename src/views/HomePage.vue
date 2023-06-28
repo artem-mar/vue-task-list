42b7e6d834ff712630877ecdb222a9191a27b05c
Artem M <artemmarin91@mail.ru>
2023-06-28 14:02:38 +0500

    added HomePage

---
+++ src/views/HomePage.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-center">
+    <h1 class="my-5">Welcome to Task Manager!</h1>
+    <router-link class="btn btn-primary" to="/tasks">Go to Task List</router-link>
+  </div>
+</template>
